2170f7b725a1b57ce5881152dff41acfffe90260
Nayak <andrescamilomaya07@gmail.com>
2026-04-14 00:21:25 -0500

    Trigger GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   root: 'frontend',
+  base: process.env.NODE_ENV === 'production' ? '/TallerReproductorDeMusica/' : '/',
   
   build: {
     outDir: '../dist/frontend',
